80100ee4f36ce3222cd8564606ad28969bdc9a5b
ThalysonDutra <70443176+ThalysonDutra@users.noreply.github.com>
2020-12-23 11:47:16 -0300

    home page build

---
+++ lib/views/drink_screen/drink.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Drink extends StatefulWidget {
+  @override
+  _DrinkState createState() => _DrinkState();
+}
+
+class _DrinkState extends State<Drink> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+
+      ),
+    );
+  }
+}
---
+++ lib/views/home_screen/home.dart
@@ -0,0 +1,133 @@
+import 'package:flutter/material.dart';
+
+int numberOfFriends = 2;
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Ra\$ha  Conta"),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Padding(
+              padding: EdgeInsets.all(20.0),
+              child: Text(
+                "Olá, bem-vindo ao Ra\$ha Conta!",
+                style: TextStyle(fontSize: 26.0, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              )),
+          Padding(
+            padding: EdgeInsets.all(20.0),
+            child: Text(
+              "Informe a quantidade de  pessoas...",
+              style: TextStyle(fontSize: 24.0),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                Padding(
+                    padding: EdgeInsets.fromLTRB(40.0, 20.0, 5.0, 0.0),
+                    child: Container(
+                      width: 60.0,
+                      height: 220.0,
+                      child: RaisedButton(
+                        onPressed: numberOfFriends ==2 ? null: () {
+                          setState(() {
+                            numberOfFriends--;
+                          });
+                        },
+                        color: Color.fromRGBO(161, 0, 0, 1),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(11.0)),
+                        child: Text(
+                          "-",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 32.0,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    )),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(0.0, 20.0, 5.0, 0.0),
+                  child: Container(
+                    width: 220,
+                    height: 220,
+                    decoration: BoxDecoration(
+                      color: Color.fromRGBO(242, 242, 242, 1),
+                      borderRadius: BorderRadius.all(
+                            Radius.circular(38.0)),
+                    ),
+                    child: Center(
+                      child: Text(numberOfFriends.toString(), style: TextStyle(fontSize: 32.0), textAlign: TextAlign.center),
+                    )
+                  ),
+                ),
+                Padding(
+                    padding: EdgeInsets.fromLTRB(0.0, 20.0, 5.0, 0.0),
+                    child: Container(
+                      width: 60.0,
+                      height: 220.0,
+                      child: RaisedButton(
+                        onPressed: () {
+                          setState(() {
+                            numberOfFriends++;
+                          });
+                        },
+                        color: Color.fromRGBO(0, 195, 20, 1),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(11.0)),
+                        child: Text(
+                          "+",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 32.0,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    )),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                Padding(
+                    padding: EdgeInsets.fromLTRB(20.0, 20.0, 5.0, 20.0),
+                    child: Container(
+                      height: 50.0,
+                      width: MediaQuery.of(context).size.width-40,
+                      child: RaisedButton(
+                        onPressed: () {
+
+                        },
+                        color: Color.fromRGBO(105, 89, 205, 1),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(11.0)),
+                        child: Text(
+                          "Continuar",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 22.0,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    )),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
